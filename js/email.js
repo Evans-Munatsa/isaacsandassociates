d85dccf3423c5633a0551b357b0a8128ca41fbc1
evansmunatsa <evansmunatsa7@gmail.com>
2025-04-08 11:17:11 +0200

    files

--- js/email.js
+++ js/email.js
@@ -1,10 +1,15 @@
 function sendMail() {
-	let params = {
-		name : document.getElementById("name").value
-		email : document.getElementById("email").value
-		subject : document.getElementById("subject").value
-		message : document.getElementById("message").value
-	}
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value
+  };
 
-	emailjs.send("service_4p1nfnh", "template_x8cdz4j", params).then(alert("Email Sent!!"))
+  emailjs.send("service_4p1nfnh", "template_x8cdz4j", params)
+    .then(function(response) {
+      alert("Email Sent!");
+    }, function(error) {
+      console.error("FAILED...", error);
+    });
 }
